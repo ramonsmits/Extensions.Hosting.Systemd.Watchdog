c534eb6b0e96ca578ab9a311351ba11047d1299f
Ramon Smits <ramon.smits@gmail.com>
2024-05-18 20:02:17 +0200

    Updated demo

--- src/Demo/Program.cs
+++ src/Demo/Program.cs
@@ -2,13 +2,25 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
-var host = WatchdogHostBuilderExtensions.UseSystemd(Host.CreateDefaultBuilder(args)).ConfigureServices(((context, services) =>
-{
-    var configuration = context.Configuration;
-    services.Configure<Options>(configuration.GetSection("Options"));
-    services.AddHostedService<Worker>();
-    services.AddSingleton<IHealthCheck, ActivityMonitor>();
-})).Build();
 
-host.Services.GetRequiredService<IOptions<Options>>();
+#if NET8_0_OR_GREATER
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.UseSystemd();
+builder.Services
+    .AddHostedService<Worker>()
+    .AddSingleton<IHealthCheck, ActivityMonitor>();
+
+#else
+var builder = Host.CreateDefaultBuilder(args)
+    .UseSystemd()
+    .ConfigureServices((context, services) =>
+    {
+        services.AddHostedService<Worker>();
+        services.AddSingleton<IHealthCheck, ActivityMonitor>();
+    });
+#endif
+
+using var host = builder.Build();
+
 await host.RunAsync();
